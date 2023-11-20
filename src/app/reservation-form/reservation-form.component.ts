4dc384c47385d709ef2dc50831e6d184d4b16d3c
Mohammad Najar <mmotieian@yahoo.com>
2023-11-19 22:15:19 -0600

    display list of reservations

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,22 +1,27 @@
 import {Component, OnInit} from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {ReservationService} from '../reservation/reservation.service';
+import {Reservation} from "../models/reservation";
+
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent implements OnInit{
+export class ReservationFormComponent implements OnInit {
 
   reservationForm: FormGroup = new FormGroup({});
 
+
   onSubmit() {
-    if(this.reservationForm.valid) {
-      console.log(this.reservationForm.value);
+    if (this.reservationForm.valid) {
+      let reservation: Reservation = this.reservationForm.value;
+      this.ReservationService.createReservation(reservation);
     }
   }
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private ReservationService: ReservationService) {
 
   }
 
@@ -28,6 +33,8 @@ export class ReservationFormComponent implements OnInit{
       guestEmail: ['', [Validators.required, Validators.email]],
       roomNumber: ['', Validators.required]
     });
+
+    // localStorage.setItem('reservations', JSON.stringify(this.ReservationService.getReservations()));
   }
 
 }
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,49 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Reservation} from "../models/reservation";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+
+  constructor() {
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
+  // CRUD
+  // Get all reservations
+  getReservations(): Reservation[] {
+    let localStorageItem = JSON.parse(localStorage.getItem('reservations')!);
+    console.log(localStorageItem);
+    return localStorageItem == null ? [] : localStorageItem.reservations;
+  }
+
+  // Get a reservation by id
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id);
+  }
+
+  // Create a new reservation
+  createReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  // Delete a reservation by id
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index, 1);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  // Update a reservation by id
+  updateReservation(reservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === reservation.id);
+    this.reservations[index] = reservation;
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
 }
